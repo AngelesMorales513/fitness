77d178da5826175268634115240dfc8601c0f567
AngelesMorales513 <56412318+AngelesMorales513@users.noreply.github.com>
2021-07-13 00:00:58 -0500

    tercer commit
    descripcion ejercicio para pierna

--- lib/screens/pierna_screen.dart
+++ lib/screens/pierna_screen.dart
@@ -9,9 +9,15 @@ class Pierna extends StatelessWidget {
       body: SingleChildScrollView(
         child: Stack(
           children: [
-            Column(
-              children: [],
-            )
+            Column(children: [
+              Image.asset(
+                "images/pierna.png",
+                width: 200,
+                height: 200,
+              ),
+              Text(
+                  "Es uno de los ejercicios más clásicos y sencillos, pero a la vez es el más potente para trabajar el tren inferior.")
+            ])
           ],
         ),
       ),
